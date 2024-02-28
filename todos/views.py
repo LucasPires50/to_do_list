e17fda333404c7ff2fffd5d9309edbc0d06d07f4
LucasPires50 <luc_50@outlook.com>
2024-02-28 16:21:49 -0300

    feat: add update, delete and complete

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,7 @@
+from datetime import date
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Todo
 
@@ -11,3 +13,20 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = ['title', 'deadline']
     success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
+        
